ab39d1363bb8f48094425c6d6c3658ab94a93c1c
KingaAndrea <rtkinga@yahoo.com>
2018-12-10 20:59:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// funtion initSkillsPage() {
+//     $.ajax('skills.json').done(funtion(skills) {
+//         var list = $('#skills-page ul');
+
+//         skills.map(function (skill) {
+//             var endorsedBy = skill.endorsedBy;
+//             if (endorsedBy) {
+//                 endorsedBy = `<span class="endorsed"> Endorsed by </span> - ${endorsedBy}`
+//             }
+//         })
+//     })
+// }
+
+$('#skills-page').show()
